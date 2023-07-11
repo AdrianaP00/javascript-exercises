a5def702151708b5652234fa7d1523300782ffa0
adriana <adrianapiccolo00@gmail.com>
2023-07-11 10:15:08 +0200

    better 7

--- S1/1.variables/ex-mine/ex7.js
+++ S1/1.variables/ex-mine/ex7.js
@@ -5,8 +5,8 @@ globalBasePrice = 25000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 
-car1.finalprice = car1.basePrice + globalBasePrice;
-car2.finalprice = car2.basePrice + globalBasePrice;
+car1.finalPrice= car1.basePrice + globalBasePrice;
+car2.finalPrice = car2.basePrice + globalBasePrice;
 
 console.log(car1.finalPrice);
 console.log(car2.finalPrice);
